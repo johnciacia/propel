33dba7361d6b763c770b4de5a5dd478a47b4528e
Spencer Hill <spencerhill@theportlandco.com>
2011-06-14 17:28:09 -0700

    - Fixed the issue with all projects being deleted instead of the selected project. - Added options for start date, end date, priority and progress.
    Signed-off-by: Spencer Hill <spencerhill@theportlandco.com>

--- propel.php
+++ propel.php
@@ -449,8 +449,8 @@ class Propel
 				break;
 				
 			case "deleteProjectAction":
-				$this->projectsModel->deleteProject($_GET['id']);
-				$this->tasksModel->deleteTasksByProject($_GET['id']);
+				$this->projectsModel->deleteProject($_GET['project']);
+				$this->tasksModel->deleteTasksByProject($_GET['project']);
 				break;
 				
 			case "createProjectAction":
