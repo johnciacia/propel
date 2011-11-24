86b1ba6c80866bbfb8abf3752939e90f01ca291b
John Ciacia <john@voceconnect.com>
2011-11-24 17:04:24 -0500

    fixed error with contributor metabox

--- metaboxes/list-authors.php
+++ metaboxes/list-authors.php
@@ -1,9 +1,7 @@
 <?php 
-$users = get_users(); 
+$users = get_users();
 $contributors = get_post_meta( get_the_ID(), '_propel_contributors');
 ?>
-
-
 <div id="propel_list_users" class="categorydiv">
 	<ul id="propel_list_users-tabs" class="category-tabs">
 		<li class="tabs">
@@ -15,13 +13,11 @@ $contributors = get_post_meta( get_the_ID(), '_propel_contributors');
 		<ul id="propel_userschecklist" class="list:propel_category categorychecklist form-no-clear">
 
 		<?php foreach($users as $user) : ?>
-		<li id="propel_user-<?php echo $user->ID; ?>" class="popular-category">
+		<li id="propel_user-<?php esc_attr_e($user->ID); ?>" class="popular-category">
 			<label class="selectit">
-				<?php if( is_array($contributors[0]) && in_array( $user->ID, $contributors[0] ) ) : ?>
-				<input value="<?php echo $user->ID; ?>" type="checkbox" name="propel_user[]" id="in-propel_user-<?php echo $user->ID; ?>" checked> <?php echo $user->display_name; ?>
-				<?php else : ?>
-				<input value="<?php echo $user->ID; ?>" type="checkbox" name="propel_user[]" id="in-propel_user-<?php echo $user->ID; ?>"> <?php echo $user->display_name; ?>
-				<?php endif; ?>
+				<input value="<?php  esc_attr_e($user->ID); ?>" type="checkbox" name="propel_user[]" id="in-propel_user-<?php echo $user->ID; ?>" <?php 
+				if( isset($contributors[0]) && in_array( $user->ID, $contributors[0] ) ) { echo "checked"; }
+				?>> <?php esc_html_e($user->display_name); ?>
 			</label>
 		</li>
 		<?php endforeach; ?>				
